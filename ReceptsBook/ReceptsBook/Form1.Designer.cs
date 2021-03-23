e497d5b015584a1c725af0625a6b53087f89f7a1
Daniil Osipov <stalkerkop@mail.ru>
2021-03-24 00:44:53 +0700

    1

---
+++ ReceptsBook/ReceptsBook/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace ReceptsBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.b_next = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.b_prev = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(307, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(283, 212);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // b_next
+            // 
+            this.b_next.Location = new System.Drawing.Point(624, 29);
+            this.b_next.Name = "b_next";
+            this.b_next.Size = new System.Drawing.Size(75, 23);
+            this.b_next.TabIndex = 0;
+            this.b_next.Text = "Next";
+            this.b_next.UseVisualStyleBackColor = true;
+            this.b_next.Click += new System.EventHandler(this.b_next_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(307, 221);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(432, 241);
+            this.textBox1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(2, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(299, 459);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(625, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0/0";
+            // 
+            // b_prev
+            // 
+            this.b_prev.Location = new System.Drawing.Point(624, 58);
+            this.b_prev.Name = "b_prev";
+            this.b_prev.Size = new System.Drawing.Size(75, 23);
+            this.b_prev.TabIndex = 6;
+            this.b_prev.Text = "Prev";
+            this.b_prev.UseVisualStyleBackColor = true;
+            this.b_prev.Click += new System.EventHandler(this.b_prev_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 468);
+            this.Controls.Add(this.b_prev);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.b_next);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button b_next;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button b_prev;
+    }
+}
+
---
+++ ReceptsBook/ReceptsBook/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Text.Json;
+using System.Web.Helpers;
+
+namespace ReceptsBook{
+    public partial class Form1 : Form
+    {
+        private static Book _book;
+        private static Recept current_recept;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string File_text = "";
+            if (File.Exists("recipes.json"))
+            {
+                File_text = File.ReadAllText("recipes.json", Encoding.GetEncoding(1251));
+            
+                dynamic json0 = Json.Decode(File_text);
+
+                _book = new Book();
+
+                foreach (dynamic jsonItem in json0.recipes)
+                {
+                    current_recept = new Recept(jsonItem.uuid,
+                                                jsonItem.name,
+                                                jsonItem.images,
+                                                jsonItem.lastUpdated,
+                                                jsonItem.description,
+                                                jsonItem.instructions,
+                                                jsonItem.difficulty);
+                
+                    _book.AddRecept(current_recept);
+                }
+
+                for(int i=0;i< _book.ind; i++)
+                {
+                    listBox1.Items.Add(_book[i].name);
+                }
+
+                listBox1.SelectedIndex = 0;
+
+                get_current_recept();
+                presentation_recept();
+            }
+            else
+                textBox1.Text = "File recipes.json don't exsist";
+        }
+
+        private Bitmap load_image_url(string url)
+        {
+            System.Net.WebRequest request = System.Net.WebRequest.Create(url);
+            System.Net.WebResponse resp = request.GetResponse();
+            System.IO.Stream respStream = resp.GetResponseStream();
+            Bitmap bmp = new Bitmap(respStream);
+            return bmp;
+        }
+        private void add_text_tb1(string text)
+        {
+            textBox1.Text += text+ Environment.NewLine;
+        }
+        private void get_current_recept()
+        {
+            int ind = listBox1.SelectedIndex;
+            current_recept = new Recept(_book[ind].uuid,
+                                        _book[ind].name,
+                                        _book[ind]._images,
+                                        _book[ind].lastUpdated,
+                                        _book[ind].description,
+                                        _book[ind].instructions,
+                                        _book[ind].difficulty);
+        }
+        private void presentation_recept()
+        {
+            add_text_tb1("<uuid>");
+            add_text_tb1(current_recept.uuid.ToString());
+
+            add_text_tb1("<name>");
+            add_text_tb1(current_recept.name.ToString());
+
+            add_text_tb1("<images>");
+            add_text_tb1(current_recept._images.ToString());
+
+            add_text_tb1("<lastUpdated>");
+            add_text_tb1(current_recept.lastUpdated.ToString());
+
+            add_text_tb1("<description>");
+            add_text_tb1(current_recept.description.ToString());
+
+            add_text_tb1("<instructions>");
+            add_text_tb1(current_recept.instructions.ToString());
+
+            add_text_tb1("<difficulty>");
+            add_text_tb1(current_recept.difficulty.ToString());
+
+            label1.Text = current_recept._images_cou.ToString()+"/" + current_recept._images_len.ToString();
+            pictureBox1.Image = load_image_url(current_recept._images[current_recept._images_cou-1]);
+
+
+        }
+        private void b_next_Click(object sender, EventArgs e)
+        {
+            current_recept._images_cou += 1;
+            if (current_recept._images_cou > current_recept._images_len)
+                current_recept._images_cou = 1;
+            label1.Text = current_recept._images_cou.ToString() + "/" + current_recept._images_len.ToString();
+            pictureBox1.Image = load_image_url(current_recept._images[current_recept._images_cou-1]);
+        }
+        private void b_prev_Click(object sender, EventArgs e)
+        {
+            current_recept._images_cou -= 1;
+            if (current_recept._images_cou < 1)
+                current_recept._images_cou = current_recept._images_len;
+            label1.Text = current_recept._images_cou.ToString() + "/" + current_recept._images_len.ToString();
+            pictureBox1.Image = load_image_url(current_recept._images[current_recept._images_cou-1]);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                textBox1.Text = "";
+                get_current_recept();
+                presentation_recept();
+            }
+        }
+
+    }
+
+    public class Recept
+    {
+        private string _uuid;
+        private string _name;
+        public dynamic _images;
+        public int _images_len;
+        public int _images_cou;
+        private int _lastUpdated;
+        private string _description;
+        private string _instructions;
+        private int _difficulty;
+
+        public string uuid => _uuid;
+        public string name => _name;
+        public int lastUpdated => _lastUpdated;
+        public string description => _description;
+        public string instructions => _instructions;
+        public int difficulty => _difficulty;
+
+        public Recept(string uuid = "",
+                        string name = "",
+                        dynamic images = null,
+                        int lastUpdated = 0,
+                        string description = "",
+                        string instructions = "",
+                        int difficulty = 0)
+        {
+            if (uuid == null)
+                uuid = "";
+            if (name == null)
+                name = "";
+            if (images == null)
+            {
+                images = new string[1];
+                images[0] = "";
+            } 
+            if (lastUpdated == null)
+                lastUpdated = 0;
+            if (description == null)
+                description = "";
+            if (instructions == null)
+                instructions = "";
+            if (difficulty == null)
+                difficulty = 0;
+
+             
+            int ind = 1;
+            foreach(var item in images)
+            {
+                ind += 1;
+            }
+
+            _uuid = uuid;
+            _name = name;
+            _images = images;
+            _images_len = ind-1;
+            _images_cou = 1;
+            _lastUpdated = lastUpdated;
+            _description = description;
+            _instructions = instructions;
+            _difficulty = difficulty;
+        }
+
+
+    }
+
+    public class Book
+    {
+
+        private List<Recept> _Book;
+        private int _ind;
+        public int ind => _ind;
+
+        public Recept this[int index] => _Book[index];
+
+        public Recept this[string name] => _Book.FirstOrDefault(g => g.name.Equals(name));
+
+        public Book()
+        {
+            _Book = new List<Recept>();
+            _ind = 0;
+        }
+
+        public void AddRecept(Recept Recept)
+        {
+            _Book.Add(Recept);
+            _ind += 1;
+        }
+
+        public void SortByName()
+        {
+            _Book = _Book.OrderBy(g => g.name).ToList();
+        }
+
+    }
+}
